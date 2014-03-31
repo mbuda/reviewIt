52d44f1012157697a70620145a5611ee7ec47592
Marta Buda <bmarta1991@o2.pl>
2014-03-31 20:02:05 +0200

    infinite scroling

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(:default, Rails.env)
 
 module Reviews
   class Application < Rails::Application
+config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -27,7 +28,6 @@ module Reviews
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-config.assets.initialize_on_precompile = false 
 
  end
 end
